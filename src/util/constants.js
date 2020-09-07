9ba19f6eaa21b9c1c6a4478f6e6be3a62edd73e6
Ryan Block <rblock@gmail.com>
2020-09-06 19:28:29 -0700

    Comments etc

--- src/util/constants.js
+++ src/util/constants.js
@@ -1,4 +1,6 @@
 module.exports = {
   apiBase: 'https://api.simplisafe.com/v1',
-  userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36'
+  userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) ' +
+             'AppleWebKit/537.36 (KHTML, like Gecko) ' +
+             'Chrome/85.0.4183.83 Safari/537.36'
 }
--- src/util/uuid.js
+++ src/util/uuid.js
@@ -1,6 +1,5 @@
 let { v4: uuidv4 } = require('uuid')
 
 module.exports = function getIDs () {
-  // Authorization
   return uuidv4()
 }
